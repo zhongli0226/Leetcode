d8f5816eb6af7e1409cae021d15191882fdd7538
zhongli0226 <794002197@qq.com>
2023-09-01 23:31:00 +0800

    2023年9月1日提交

--- 13.罗马数字转整数.py
+++ 13.罗马数字转整数.py
@@ -11,5 +11,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        dic = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        res = 0
+        for i in range(len(s)):
+            if i > 0 and dic[s[i]] > dic[s[i-1]]:
+                res += dic[s[i]] - 2 * dic[s[i-1]]
+            else:
+                res += dic[s[i]]
+        return res
 # @lc code=end
 
