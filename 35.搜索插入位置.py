23c2fd306c2566370c098feadc88b56c7bce195f
zhongli0226 <794002197@qq.com>
2023-09-03 20:30:56 +0800

    2023年9月3日提交

---
+++ 35.搜索插入位置.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=35 lang=python
+#
+# [35] 搜索插入位置
+#
+
+# @lc code=start
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums[i]>=target:
+                return i
+        return len(nums)
+# @lc code=end
+
